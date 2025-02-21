12639aa4c4e90ad458b3bb0cbaaecb091c66b13a
prideandjuice <elvirarahma07@gmail.com>
2025-02-21 12:51:51 +0700

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:skap/screens/signup_screen.dart';
 import 'screens/home_screen.dart';
 import 'screens/home_page.dart';
 import 'package:skap/screens/next_signup.dart';
+import 'screens/dashboard.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,16 +15,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'SKAP',
-      theme: ThemeData(primarySwatch: Colors.purple),
-      initialRoute: '/',
-      routes : {
-        '/': (context) => const HomeScreen(),
-        '/home' : (context) => const HomePage(),
-        '/signup' : (context) => SignUpScreen(),
-        '/next_signup' : (context) => NextSignUpScreen(),
-      }
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'SKAP',
+        theme: ThemeData(primarySwatch: Colors.purple),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const HomeScreen(),
+          '/home': (context) => const HomePage(),
+          '/signup': (context) => SignUpScreen(),
+          '/next_signup': (context) => NextSignUpScreen(),
+          '/dashboard' : (context) => Dashboard(),
+        });
   }
 }
